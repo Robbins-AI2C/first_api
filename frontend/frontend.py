8742dcbd3ca700174008437b7e39cc56ca937955
ncvarsity3 <ncvarsity3@gmail.com>
2024-12-29 11:09:13 -0600

    deleted example functionalities

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -19,26 +19,3 @@ if st.button("Google Search"):
         else:
             st.error("Error: Could not process request.")
 
-
-
-
-# Input text box
-input_text = st.text_input("Enter test text:")
-input_text = input_text.replace(" ", "+")
-
-# Button to submit text
-if st.button("Reverse Text"):
-    if input_text:
-        # Send request to FastAPI backend
-        backend_url = "http://localhost:8000/reverse"  # URL of the FastAPI backend
-        response = requests.post(backend_url, json={"text": input_text})
-
-        # Process response
-        if response.status_code == 200:
-            new_text = response.json().get("reversed_text")
-            st.success(f"Reversed Text: {new_text}")
-        else:
-            st.error("Error: Could not process request.")
-    else:
-        st.warning("Please enter some text.")
-
